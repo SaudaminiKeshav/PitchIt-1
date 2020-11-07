f592a1c7e04f3282a53396b963c4c2cd8637e06b
Corey Burkett <coreyburkett22@gmail.com>
2020-11-07 12:54:12 -0800

    created trip card model and still working on the get/ post routes

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const trips = require("./routes/api/trips");
 const app = express();
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
+
 app.use(bodyParser.json());
+
 // DB Config
 const db = process.env.MONGODB_URI;
+
 // Connect to MongoDB
 mongoose.connect(
   db || "mongodb://localhost/5000", {
@@ -20,18 +25,17 @@ mongoose.connect(
   useCreateIndex: true,
   useFindAndModify: false
 }).then(() => console.log("MongoDB successfully connected"))
-.catch(err => console.log(err+"Error while connecting to mongo !!!!"));
-// mongoose.connect(
-//     db || 'mongodb://localhost/',
-//     { useNewUrlParser: true }
-//   )
-//   .then(() => console.log("MongoDB successfully connected"))
-//   .catch(err => console.log(err));
-    // Passport middleware
+.catch(err => console.log(err + "Error while connecting to mongo !!!!"));
+
+// Passport middleware
 app.use(passport.initialize());
+
 // Passport config
 require("./config/passport")(passport);
+
 // Routes
 app.use("/api/users", users);
+app.use("/api/trips", trips);
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
