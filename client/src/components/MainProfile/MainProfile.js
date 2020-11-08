f2390af2689f771f8d62ac2983256dfffe586332
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-11-07 19:17:03 -0800

    Profile page style update

--- client/src/components/MainProfile/MainProfile.js
+++ client/src/components/MainProfile/MainProfile.js
@@ -1,17 +1,4 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-
-const contentAreaStyle = {
-    backgroundColor: "white",
-    width: "75%",
-    height: "300px",
-    marginLeft: "20%",
-    top: "40%",
-    position: "absolute",
-    borderRadius: "30px",
-    boxShadow: "0px 0px 5px rgba(0,0,0,.6)",
-    fontWeight: "900",
-}
 
 const MainProfile = (props) => {
     console.log(props.userSelection)
@@ -19,9 +6,9 @@ const MainProfile = (props) => {
     let userSelectedOption = props.userSelection;
 
     return <>
-        <div className="container" style={contentAreaStyle}>
-            <h1 style={{textAlign:"center" }}>{userSelectedOption}</h1>
-        <hr/>
+        <div className="contentAreaStyle">
+            <h1 style={{ textAlign: "center" }}>{userSelectedOption}</h1>
+            <hr />
         </div>
     </>;
 }
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -23,11 +23,11 @@ const navTabBtns = {
 
 const Navbar = () => {
     return (
-        <nav style={ navBackgroundStyle } className="navbar navbar-expand-lg navbar-light">
+        <nav style={navBackgroundStyle} className="navbar navbar-expand-lg navbar-light">
             <li className="brand nav-link">
                 <img src="img/campfire.png" alt="logo" className="logo" width="50px" />
             </li>
-            <a style={ navTitleBtnStyle } className="navbar-brand" href="">PitchIt</a>
+            <a style={navTitleBtnStyle} className="navbar-brand" href="">PitchIt</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -51,7 +51,7 @@ const Navbar = () => {
                     </li>
                 </ul>
                 <li className="navbar-nav nav-item sign-out-btn">
-                        <a style={navTabBtns} className="nav-link" href="">Sign Out</a>
+                    <a style={navTabBtns} className="nav-link" href="">Sign Out</a>
                 </li>
                 {/* <form className="form-inline my-2 my-lg-0">
                     <button className="btn btn-outline-warning my-2 my-sm-0" type="submit">Sign Out</button>
