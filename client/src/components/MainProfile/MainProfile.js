1b6074f418b0f711a4eb76039ecfa2a3aaa3d95b
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-11-07 19:03:20 -0800

    Profile page style update

--- client/src/components/MainProfile/MainProfile.js
+++ client/src/components/MainProfile/MainProfile.js
@@ -1,35 +1,27 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 const contentAreaStyle = {
     backgroundColor: "white",
     width: "75%",
+    height: "300px",
     marginLeft: "20%",
-    top: "35%",
+    top: "40%",
     position: "absolute",
     borderRadius: "30px",
-    boxShadow: "1.5px 1.5px 3px rgba(1,1,1,.5)",
+    boxShadow: "0px 0px 5px rgba(0,0,0,.6)",
     fontWeight: "900",
 }
 
-// const tripStyle = {
-//     textAlign: "center",
-//     borderRadius: "10%",
-//     backgroundColor: "orange"
-// }
-
-// const mileStoneStyle = {
-//     textAlign: "center",
-//     borderRadius: "50%",
-//     backgroundColor: "green"
-// }
-
 const MainProfile = (props) => {
     console.log(props.userSelection)
-    let userName = props.user.name
+    let userName = props.user.name;
+    let userSelectedOption = props.userSelection;
 
     return <>
-        <div style={contentAreaStyle}>
-            <h1 style={{ height: "300px" }}>{userName}</h1>
+        <div className="container" style={contentAreaStyle}>
+            <h1 style={{textAlign:"center" }}>{userSelectedOption}</h1>
+        <hr/>
         </div>
     </>;
 }
--- client/src/components/MainProfile/Sidebar.js
+++ client/src/components/MainProfile/Sidebar.js
@@ -3,37 +3,11 @@ import tripIcon from "../../img/trips-icon.png";
 import reviewIcon from "../../img/review-icon.png";
 import badgeIcon from "../../img/badge-icon.png";
 import placeHolder from "../../img/placeholder.png";
+import arrowIcon from "../../img/arrowIcon.png";
 import "../MainProfile/style.css"
 import classNames from 'classnames'
 
-const sideBarStyle = {
-    float: "left",
-    width: "12%",
-    height: "10%",
-    marginTop: "10%",
-    fontWeight: "900",
-    fontSize: "large",
-
-    marging: "5%",
-    marginLeft: "4%",
-    textAlign: "center"
-}
-
-const iconStyle = {
-    height: "15%",
-    width: "15%",
-    margin: "10%"
-}
-const OptionsTextAlign = {
-    textAlign: "start",
-    fontSize: "100%",
-    fontWeight: "795",
-}
-
-// let classes = classNames.bind(styles);
-
 const SideBar = (props) => {
-    // const [sideBarOption, setSideBarOption] = useState('');
     console.log(props.user);
 
     function displayTrips() {
@@ -50,25 +24,28 @@ const SideBar = (props) => {
 
     return <>
         <section>
-            <div style={sideBarStyle}>
+            <div className="sideBarStyle">
                 <img src={placeHolder} className="profilePicStyle" alt="profile" />
                 <ul>
-                    <li className={classNames('sideBarMenuItem', 'tripsStyle')} onClick={displayTrips.bind(this)}>
-                        <h5 style={OptionsTextAlign}>
-                            <img src={tripIcon} style={iconStyle} alt="cannot display" />
+                    <li className={classNames('sideBarMenuItem', props.userSelection == "Trips" ? 'tripActive' : 'tripsStyle')} onClick={displayTrips.bind(this)}>
+                        <h5 className="optionsTextAlign">
+                            <img src={tripIcon} className="iconStyle" alt="cannot display" />
                             Trips
+                            <img src={arrowIcon} className="arrowIconStyle" alt="profile" />
                         </h5>
                     </li>
-                    <li className={classNames('sideBarMenuItem', 'reviewStyle')} onClick={displayReviews.bind(this)}>
-                        <h5 style={OptionsTextAlign}>
-                            <img src={reviewIcon} style={iconStyle} alt="cannot display" />
+                    <li className={classNames('sideBarMenuItem', props.userSelection == "Reviews" ? 'revewActive' : 'reviewStyle')} onClick={displayReviews.bind(this)}>
+                        <h5 className="optionsTextAlign">
+                            <img src={reviewIcon} className="iconStyle" alt="cannot display" />
                             Reviews
+                            <img src={arrowIcon} className="arrowIconStyle" alt="profile" />
                         </h5>
                     </li>
-                    <li className={classNames('sideBarMenuItem', 'mileStoneStyle')} onClick={displayMilestones.bind(this)}>
-                        <h5 style={OptionsTextAlign}>
-                            <img src={badgeIcon} style={iconStyle} alt="cannot display" />
+                    <li className={classNames('sideBarMenuItem', props.userSelection == "Milestones" ? 'milestoneActive' : 'mileStoneStyle')} onClick={displayMilestones.bind(this)}>
+                        <h5 className="optionsTextAlign">
+                            <img src={badgeIcon} className="iconStyle" alt="cannot display" />
                             Milestones
+                            <img src={arrowIcon} className="arrowIconStyle" alt="profile" />
                         </h5>
                     </li>
                 </ul>
