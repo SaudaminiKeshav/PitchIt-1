240e080d8a8cca43d87c1d6b0a65184c3c739ad6
Corey Burkett <coreyburkett22@gmail.com>
2020-11-16 13:42:21 -0800

    Added trip cards and top locations to dashboard

--- client/src/components/MainProfile/Trips.js
+++ client/src/components/MainProfile/Trips.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 
-const profileAreaStyle = {
-    backgroundColor: "orange"
-}
+// const profileAreaStyle = {
+//   backgroundColor: "orange"
+// }
 
 // const tripStyle = {
 //   textAlign: "center",
@@ -30,24 +30,40 @@ const Trips = (props) => {
 
 
   return <>
-  <div style={ profileAreaStyle }>
+  {/* <div style={ profileAreaStyle }>
     {trips.map((each, index)=>{
       return (
         <div>
-      <div class="card" style={{width: "18rem"}}>
-        <div class="card-body">
-      <h5 class="card-title">{each.title}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">{each.location}</h6>
-          <p class="card-text">{each.date}</p>
-          <button>View Details</button>
-          <button>Edit</button>
+          <div class="card" style={{width: "18rem"}}>
+            <div class="card-body">
+              <h5 class="card-title">{each.title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{each.location}</h6>
+              <p class="card-text">{each.date}</p>
+              <button>View Details</button>
+              <button>Edit</button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
       )
     })}
     
-  </div>
+  </div> */}
+              {trips.map((each, index)=>{
+                return (
+                    <div className="col mb-4">
+                        <div className="card h-100">
+                            <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{each.title}</h5>
+                                <p className="card-text">{each.date}</p>
+
+                                <button>View Details</button>
+                                <button>Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
   </>;
 }
 
