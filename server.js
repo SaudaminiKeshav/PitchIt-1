cb2b25048c206d26714c45c3992659e247a9909b
Juliana Andrews <julianakandrews@gmail.com>
2020-11-18 20:11:57 -0800

    Partial upload profile picture, changing mongodb

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ require('dotenv').config();
 const CreateTripModel = require("./models/CreateTrip.js");
 const User = require("./models/User");
 
+//ADDED NEW STUFF START
+const path = require("path");
+const multer = require("multer");
+const cors = require("cors");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const crypto = require("crypto");
+//ADDED NEW STUFF END
+
 // DB Config
 const db = process.env.mongoURI;
 const apiKey = process.env.apiKey;
@@ -18,13 +27,21 @@ mongoose.connect(
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
-}).then(() => console.log("MongoDB successfully connected"))
+}).then(() => 
+//ADDED NEW STUFF START
+{
+  gfs = Grid(conn.db, mongoose.mongo)
+  gfs.collection('uploads')
+  console.log("MongoDB successfully connected")
+}
+//ADDED NEW STUFF END
+)
 .catch(err => console.log(err + "Error while connecting to mongo !!!!"));
 
 // Bodyparser middleware
-app.use(bodyParser.json({limit: '2000kb', extended: true}));
+app.use(bodyParser.json({limit: "500kb"}));
 app.use(
-  bodyParser.urlencoded({limit: '2000kb',
+  bodyParser.urlencoded({ limit: "500kb",
     extended: false
   })
 );
@@ -115,6 +132,58 @@ app.put("/api/profile", (req, res) => {
   });
 });
 
+//ADDED NEW STUFF START
+// Create storage engine
+const storage = new GridFsStorage({
+  url: db,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err)
+        }
+        const filename = file.originalname
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads',
+        }
+        resolve(fileInfo)
+      })
+    })
+  },
+})
+
+const upload = multer({ storage })
+
+app.post('/', upload.single('img'), (req, res, err) => {
+  if (err) throw err
+  res.status(201).send()
+})
+
+let gfs;
+
+app.get('/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists',
+      })
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename)
+      readstream.pipe(res)
+    } else {
+      res.status(404).json({
+        err: 'Not an image',
+      })
+    }
+  })
+})
+//ADDED NEW STUFF END
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
