723d21948cb8af97060bc2addc1cf0d1b67ee7b4
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-11-20 17:34:46 -0800

    Updated the profile pic upload feature

--- client/src/components/MainProfile/ProfilePicture.js
+++ client/src/components/MainProfile/ProfilePicture.js
@@ -1,45 +1,61 @@
 import React, { Component } from "react";
 import "../MainProfile/style.css";
+import classNames from 'classnames';
 
 import placeHolder from "../../img/placeholder.heic";
 //import axios from 'axios';
 
 class ProfilePicture extends Component {
-    //const user = this.props.user;
+  constructor(props){
+    super(props)
+    this.state = {
+      file: null
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-    Post = event => {
-        event.preventDefault();
-        const file = document.getElementById("add-image-input").files;
-        const formData = new FormData();
-    
-        formData.append("img", file[0]);
-    
-        fetch("http://localhost:5000/", {
-          method: "POST",
-          body: formData
-        }).then(res => {
-          console.log(res);
-        });
-    
-        document.getElementById("img").setAttribute("src", `http://localhost:5000/${file[0].name}`);
-        console.log(file[0]);
-      };
+  handleChange(event) {
+    this.setState({
+      file: URL.createObjectURL(event.target.files[0])
+    })
+  }
 
+  Post = event => {
+    event.preventDefault();
+    const file = document.getElementById("add-image-input").files;
+    const formData = new FormData();
 
-    //<img src={placeHolder} onClick={() => this.fileInput.click()} className="profilePicStyle" alt="profile" />
-    // <img src={this.state.selectedFile || placeHolder} className="profilePicStyle" alt="profile" />
-render () { 
-    return( <>
-    <div className="img-upload">
-        {/* <input type="file" className="add-image" id="add-image-input" aria-describedby="add-image-input" />
-        <label className="add-image-label" htmlFor="add-image-input">Choose File</label>
+    formData.append("img", file[0]);
+
+    fetch("http://localhost:5000/", {
+      method: "POST",
+      body: formData
+    }).then(res => {
+      console.log(res);
+    });
+
+    document.getElementById("img").setAttribute("src", `http://localhost:5000/${file[0].name}`);
+    console.log(file[0]);
+  };
+
+  render() {
+    return (<>
+      {/* <div className="img-upload">
+        <input type="file" className=""  />
         <button type="button" className="btn btn-primary" onClick={this.Post}>Upload</button>
-        <hr /> */}
-        <img src={placeHolder} className="profilePicStyle" alt="profile" />
-    </div>
+        <hr />
+        <img src={placeHolder}  id="add-image-input" className={classNames('profilePicStyle', 'add-image-label')} htmlFor="add-image-input" aria-describedby="add-image-input"  alt="profile" />
+    </div> */}
+      <label htmlFor="myInput"> <img src={this.state.file ? this.state.file :placeHolder} type="camera" id="profileImage" className={classNames('profilePicStyle', 'add-image-label')} htmlFor="add-image-input" aria-describedby="add-image-input" alt="profile" /></label>
+      <input
+        id="myInput"
+        style={{ display: 'none' }}
+        type={"file"}
+        onChange={this.handleChange}
+      />
     </>
     );
-} 
+  }
 }
 
 export default ProfilePicture;
