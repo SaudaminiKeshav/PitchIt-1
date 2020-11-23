06bc203b8af83ae3be763c917988522b30c5e05e
Harold Zuluaga <haroldzuluaga@aol.com>
2020-11-22 22:17:48 -0800

    updated server for heroku only

--- server.js
+++ server.js
@@ -193,55 +193,55 @@ app.put('/api/user/:id', (req, res, err) => {
 })
 
 // Create storage engine
-const storage = new GridFsStorage({
-  url: db,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err)
-        }
-        const filename = file.originalname
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads',
-        }
-        resolve(fileInfo)
-      })
-    })
-  },
-})
+// const storage = new GridFsStorage({
+//   url: db,
+//   file: (req, file) => {
+//     return new Promise((resolve, reject) => {
+//       crypto.randomBytes(16, (err, buf) => {
+//         if (err) {
+//           return reject(err)
+//         }
+//         const filename = file.originalname
+//         const fileInfo = {
+//           filename: filename,
+//           bucketName: 'uploads',
+//         }
+//         resolve(fileInfo)
+//       })
+//     })
+//   },
+// })
 
-const upload = multer({ storage })
+// const upload = multer({ storage })
 
-app.post('/', upload.single('progileImg'), (req, res, err) => {
-  if (err) throw err
-  res.status(201).send()
-})
+// app.post('/', upload.single('progileImg'), (req, res, err) => {
+//   if (err) throw err
+//   res.status(201).send()
+// })
 
-let gfs;
+// let gfs;
 
-app.get('/:filename', (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists',
-      })
-    }
+// app.get('/:filename', (req, res) => {
+//   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+//     // Check if file
+//     if (!file || file.length === 0) {
+//       return res.status(404).json({
+//         err: 'No file exists',
+//       })
+//     }
 
-    // Check if image
-    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-      // Read output to browser
-      const readstream = gfs.createReadStream(file.filename)
-      readstream.pipe(res)
-    } else {
-      res.status(404).json({
-        err: 'Not an image',
-      })
-    }
-  })
-})
+//     // Check if image
+//     if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+//       // Read output to browser
+//       const readstream = gfs.createReadStream(file.filename)
+//       readstream.pipe(res)
+//     } else {
+//       res.status(404).json({
+//         err: 'Not an image',
+//       })
+//     }
+//   })
+// })
 //ADDED NEW STUFF END
 
 // let router = express.Router();
