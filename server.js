b258ce68c8e9dc5d9871874c7af5bf74bdd70ac0
Harold Zuluaga <haroldzuluaga@aol.com>
2020-11-23 01:06:21 -0800

    server.js GET

--- server.js
+++ server.js
@@ -189,7 +189,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express serve up index.html file if it doesn't recognize route
   // const path = require('path');
   app.use(routes)
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile('index.html');
   });
 }
